772a061f49193a16a2339dcf0c70e3c43950b472
Philippe De Baer <philippe.de.baer@thomascook.be>
2018-02-28 09:23:58 +0100

    ps test script

---
+++ CRM.PowerShell.Deployment/SetBusinessRuleOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.PowerShell.Deployment
+{
+    [Cmdlet(VerbsCommon.Set, "BusinessRuleOrder")]
+    [OutputType(typeof(string))]
+    public class SetBusinessRuleOrderCmdlet : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string CrmSourceConnectionString
+        {
+            get { return crmSourceConnectionString; }
+            set { crmSourceConnectionString = value; }
+        }
+        private string crmSourceConnectionString;
+
+        [Parameter(Position = 0, Mandatory = true)]
+        public string CrmTargetConnectionString
+        {
+            get { return crmTargetConnectionString; }
+            set { crmTargetConnectionString = value; }
+        }
+        private string crmTargetConnectionString;
+
+        protected override void ProcessRecord()
+        {
+            
+
+
+            WriteObject("Updated version info");
+        }
+    }
+}
--- CRM.PowerShell/DeploymentTest.ps1
+++ CRM.PowerShell/DeploymentTest.ps1
@@ -1,4 +1,7 @@
-﻿Set-Location -Path 'D:\Development\Test\Dyn365 Sandbox'
+﻿#	TEST DEPLOYMENT CMDLETS
+#	***********************
+
+Set-Location -Path 'D:\Development\Test\Dyn365 Sandbox'
 Remove-Module CRM.PowerShell.Deployment
 Import-Module .\CRM.PowerShell.Deployment\bin\Debug\CRM.PowerShell.Deployment.dll -Force
 Get-Command -module CRM.PowerShell.Deployment
